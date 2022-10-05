c4cbd4057517ce804161926f41eb4d08c42de21e
Luan Fernandes <luanfern2004@gmail.com>
2022-10-05 15:33:11 -0300

    first migration

---
+++ prisma/migrations/20221005131932_firtsmigrate/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "telefone" TEXT NOT NULL,
+    "saldo" DOUBLE PRECISION NOT NULL DEFAULT 0.00,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_telefone_key" ON "users"("telefone");
